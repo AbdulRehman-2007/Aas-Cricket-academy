528fec88ceefd4b1ca84486991b3e51d5f1b54cc
Abdul Rehman <a7735432@gmail.com>
2025-09-30 01:44:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Navbar Toggle
+const toggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
